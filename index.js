3dff1274ee89b660b337da634e929fa5fefc5497
tull3 <code@thinke.dev>
2022-10-19 18:22:04 -0400

    Reorganize

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const os = require('node:os');
+const { exec } = require('node:child_process');
+
+const say = console.log;
+const log = (err, stdout, stderr) => {
+    if (err) {
+        console.error(`exec error: ${err}`);
+        return;
+    }
+    console.log(stdout);
+}
+
+exec('cat /etc/centos-release', log);
+
+say(process.argv);
--- linux/setup-centos.sh
+++ linux/setup-centos.sh
@@ -14,7 +14,8 @@ sudo hostnamectl set-hostname example-hostname
 
 sudo dnf install git \
 	tar \
-	java-17-openjdk-devel
+	java-17-openjdk-devel \
+	nodejs
 
 curl https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz --output maven.tar.gz
 tar -xvf maven.tar.gz
